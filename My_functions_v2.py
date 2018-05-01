e5e43904bc2b2ce15dc5e230b2684d5772b008a9
s153283 <simon.kok@live.dk>
2018-05-01 19:50:29 +0200

    Changes to use FAC dual products. And add my function vision 2 for that. Load function use ls and does not take days as an input

---
+++ My_functions_v2.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 17 12:46:35 2018
+
+@author: simon
+"""
+
+## Basic functionality
+import swarmtoolkit as st
+import numpy as np
+import os
+import pandas as pd
+
+
+#%%
+
+def load_DNS():
+    # This function loads the DNS zip files from data_loc
+    # It loads for x days in march 2015
+    # The output is a list with swarmtoolkit.sw_io.Parameter
+   
+    # path of data
+    data_loc = "/home/simon/Desktop/Bachelor_project/data/DNSCWND/"
+    
+    files=os.listdir(data_loc)
+    dates = np.array(range(1,31+1))
+    
+    error_count = 0
+    
+    for i in range(0,len(files)):
+        file_name = files[i]
+        ##Load data
+        try:
+            temp_DNS = st.getCDFparams(data_loc+file_name,temp=True)
+            
+            if i-error_count == 0:
+                 DNS = temp_DNS 
+            else:
+                ## Append data
+                for k in range(len(DNS)):
+                    DNS[k].values= np.append(DNS[k].values,temp_DNS[k].values)
+            file_date = file_name[25:27]
+            dates[dates==int(file_date)] = 0
+        except:
+            print(file_name+': Could not be loaded')
+            error_count += 1
+    
+    fails = dates[dates != 0]
+    if len(fails) != 0:
+        print('Error loading files for these dates:')
+        print(fails)
+    
+    ## Fix unrealistic values / errors in density
+    DNS[5].values[DNS[5].values > 1e30] = float('nan')    
+    
+    # Convert to panda data format
+    dates=pd.to_datetime(DNS[0].values)
+    data = [DNS[1].values]
+    names = ['Altitude','Latitude','Longitude','Local_solar_time','Density']
+    for i in range(2,len(DNS)):
+        data.append(DNS[i].values)
+    DNS = pd.DataFrame(np.transpose(data), index=dates, columns=names)
+    
+    DNS = DNS.sort_index()
+    
+    return DNS
+
+#%%
+    
+def load_FAC(sat='dual'):
+    # This function loads the FAC zip files from data_loc
+    # It loads for x days in march 2015
+    # The output is a list with swarmtoolkit.sw_io.Parameter
+    # !!!Note that the 5 march of sat='A' is not in the folder!!!
+    
+    if sat == 'dual':
+        # path of data
+        data_loc = "/home/simon/Desktop/Bachelor_project/data/FAC_dual/"
+    elif sat == 'A':
+        # path of data
+        data_loc = "/home/simon/Desktop/Bachelor_project/data/FAC_A/"
+    elif sat =='C':
+        # path of data
+        data_loc = "/home/simon/Desktop/Bachelor_project/data/FAC_C/"
+    error_count = 0
+    
+    files=os.listdir(data_loc)
+    dates = np.array(range(1,31+1))
+    
+    
+    for i in range(0,len(files)):
+        file_name = files[i]
+        ##Load data
+        try:
+            temp_FAC = st.getCDFparams(data_loc+file_name,temp=True)
+            
+            if i-error_count == 0:
+                 FAC = temp_FAC 
+            else:
+                ## Append data
+                for k in range(len(FAC)):
+                    FAC[k].values= np.append(FAC[k].values,temp_FAC[k].values)
+            file_date = file_name[25:27]
+            dates[dates==int(file_date)] = 0
+        except:
+            print(file_name+': Could not be loaded')
+            error_count += 1
+    
+    fails = dates[dates != 0]
+    if len(fails) != 0:
+        print('Error loading files for these dates:')
+        print(fails)
+    
+    # convert to pandas
+    dates=pd.to_datetime(FAC[0].values)
+    data = [FAC[1].values]
+    names = [FAC[1].name]
+    for i in range(2,len(FAC)):
+        data.append(FAC[i].values)
+        names.append(FAC[i].name)
+    FAC = pd.DataFrame(np.transpose(data), index=dates, columns=names)
+    FAC = FAC.sort_index()
+    return FAC
+
+#%%
+    
+def add_orbit(dataframe):
+    """
+    Output an np.array with the orbit nr of corrospondig to the latitude.
+    The firt mesument is denoted 0 the next 1 and so on.
+    Be aware that the latitude should be ordered in time. 
+    !!! Orbit nr is sensitive to holds in time series. Orbit nr does not 
+    align when there is holds!!!
+    
+    """
+    # initial orbits
+    latitude = dataframe['Latitude']
+    orbits = np.zeros(len(latitude))
+    hemisphere = np.ones(len(latitude))
+    current_orbit = 0
+    
+    # Check if first mesument is on the southen hemisphere
+    if latitude[0]<0:
+        hemisphere[0]=-1
+        
+    # go through all latitudes
+    for i in range(1,len(latitude)):
+        
+        if latitude[i]>0:
+            hemisphere[i]=1 # set to norhten hemiphere 
+            # If the Acending node is crossed. New orbit
+            if latitude[i-1]<0:
+                current_orbit += 1
+        else:
+            hemisphere[i]=-1 # set to southen hemiphere 
+        # sets orbit
+        orbits[i] = current_orbit
+        
+    dataframe.loc[:,'Orbit_nr'] =  orbits
+    dataframe.loc[:,'Hemisphere'] = hemisphere
+    return None
+            
+#%%         
+
+def orbit_means(dataframe,mode='abs'):
+    
+    if ('FAC' in dataframe.columns):
+        pos = 'FAC'
+    elif ('Density' in dataframe.columns):
+        pos = 'Density'
+    else:
+        print('Error in data type')
+        return 0
+    
+    if not 'Orbit_nr' in dataframe.columns:
+        add_orbit(dataframe)
+
+
+    #get orbit nr.
+    orbit_nr = np.repeat(np.array(range(int(dataframe.Orbit_nr[-1]+1))),2)
+    hemisphere = -1*np.ones(len(orbit_nr))
+    hemisphere[::2]=-hemisphere[::2]
+    #Intialize arrays for result
+    values = np.zeros(len(orbit_nr))
+    mesuments = np.zeros(len(orbit_nr))
+    delta_time = [0 for x in range(len(orbit_nr))]
+    dates = [0 for x in range(len(orbit_nr))]
+    # sets first orbit
+    
+    # Go through data
+    for i in range(len(orbit_nr)):
+        # get the orbit
+        df = dataframe[dataframe.Orbit_nr == orbit_nr[i]]
+        # Get hemisphere
+        df = df[df.Hemisphere == hemisphere[i]]
+          
+        mesuments[i] = len(df.loc[:,pos])
+        if mesuments[i] == 0:
+            values[i] = float('nan')
+            delta_time[i] = 0
+            dates[i] = dataframe.index[0]
+        else:
+            delta_time[i] = (df.index[-1]-df.index[0]).total_seconds()
+            dates[i] = df.index[0] + (df.index[-1]-df.index[0])/2
+            if mode == 'simple':
+               values[i] = df.loc[:,pos].mean()
+            if mode == 'abs':
+                values[i] = df.loc[:,pos].apply(abs).mean()
+    
+            if mode == 'power':
+                values[i] = np.sqrt(df.loc[:,pos].apply(lambda x: x**2).mean())
+    data = [values,orbit_nr,hemisphere,mesuments,delta_time]
+    names = [pos,'Orbit_nr','Hemisphere','Count','Delta_time']
+    means = pd.DataFrame(np.transpose(data), index=dates, columns=names)
+    return means
+    
---
+++ NRLMSISE.m
@@ -0,0 +1,4 @@
+clc
+%% nrlmsise test
+
+[T rho] = atmosnrlmsise00( 10000, 45, -50, 2007, 4, 0)
